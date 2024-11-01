a856ca7a995f009b13a181d7492898bde50540ea
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2024-10-31 23:18:02 -0500

    c

--- projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/configuration/AppConfig.java
+++ projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/configuration/AppConfig.java
@@ -1,7 +1,21 @@
 package com.maverick.projectManagementSystem.configuration;
 
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.sessionManagement(Management-> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize-> Authorize.requestMatchers("/api/**").authenticated())
+
+        return http.build();
+    }
 }
