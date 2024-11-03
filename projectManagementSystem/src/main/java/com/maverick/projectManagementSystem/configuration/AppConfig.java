03f2ea4257b2e8c1963a95c121a5026b2b6820cf
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2024-11-03 15:48:12 -0600

    add cors, security filterchain, jwt validation

--- projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/configuration/AppConfig.java
+++ projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/configuration/AppConfig.java
@@ -1,11 +1,20 @@
 package com.maverick.projectManagementSystem.configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import java.util.*;
+
 
 @Configuration
 @EnableWebSecurity
@@ -13,9 +22,42 @@ public class AppConfig {
 
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http.sessionManagement(Management-> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(Authorize-> Authorize.requestMatchers("/api/**").authenticated())
+        http.sessionManagement((session)-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //because we are going to use JWT for each request
+                .authorizeHttpRequests((authorize)-> authorize
+                                                        .requestMatchers("/api/**").authenticated() //every request needs to be authenticated using jwt
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration ccfg = new CorsConfiguration();
+                ccfg.setAllowedOrigins(Arrays.asList(
+                    "http://localhost:3000",
+                    "http://localhost:5173", //5173 is for React with Vita
+                    "http://localhost:4200" //4200 is for angular
+                        //provide the deployed frontend url also when frontend is launched.
+                ));
+
+                ccfg.setAllowedMethods(Collections.singletonList("*"));
+                ccfg.setAllowCredentials(true);
+                ccfg.setAllowedHeaders(Collections.singletonList("*"));
+                ccfg.setExposedHeaders(Arrays.asList("Authorization"));
+                ccfg.setMaxAge(3600L);
+                return null;
+            }
+        };
+    }
+
+    //decryp the user password
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/configuration/JwtConstants.java
@@ -0,0 +1,7 @@
+package com.maverick.projectManagementSystem.configuration;
+
+public class JwtConstants {
+
+    public static final String SECRET_KEY = "kaleouiandvlk4098qoiglkadnf";
+    public static final String JWT_HEADER = "Authorization";
+}
