a732c4e1733dd91a4c953075a6c7b975dfe0ea17
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2024-11-26 11:33:45 -0600

    issue api

--- projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/controller/IssueController.java
+++ projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/controller/IssueController.java
@@ -1,15 +1,17 @@
 package com.maverick.projectManagementSystem.controller;
 
 import com.maverick.projectManagementSystem.model.Issue;
+import com.maverick.projectManagementSystem.model.User;
+import com.maverick.projectManagementSystem.model.dto.IssueDTO;
+import com.maverick.projectManagementSystem.request.IssueRequest;
+import com.maverick.projectManagementSystem.response.AuthResponse;
+import com.maverick.projectManagementSystem.response.MessageResponse;
 import com.maverick.projectManagementSystem.service.IssueService;
 import com.maverick.projectManagementSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,15 +29,68 @@ public class IssueController {
     public ResponseEntity<Issue> fetchIssueById(
             @PathVariable Long issueId
     ) throws Exception{
-        Issue iss = issueService.getIssueById(issueId);
-        return new ResponseEntity<>(iss, HttpStatus.OK);
+//        Issue iss = issueService.getIssueById(issueId);
+//        return new ResponseEntity<>(iss, HttpStatus.OK);
+        return ResponseEntity.ok(issueService.getIssueById(issueId));
     }
 
     @GetMapping("/project/{projectId}")
-    public ResponseEntity<List<Issue>> fetchIssuesByProjectId(@PathVariable Long projectId) throws Exception{
+    public ResponseEntity<List<Issue>> fetchIssuesListByProjectId(@PathVariable Long projectId) throws Exception{
 
         List<Issue> liss = issueService.getIssueByProjectId(projectId);
         return new ResponseEntity<>(liss, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public  ResponseEntity<IssueDTO> createIssue(@RequestBody IssueRequest issue,
+                                                 @RequestHeader("Authorization") String token)
+                                                    throws Exception{
+
+        User tokenUser = userService.findUserProfileByJwt(token);
+        User user = userService.findUserById(tokenUser.getId());
+
+            Issue createdIssue = issueService.createIssue(issue, tokenUser);
+            IssueDTO issueDTO = new IssueDTO();
+            issueDTO.setDescription(createdIssue.getDescription());
+            issueDTO.setId(createdIssue.getId());
+            issueDTO.setDueDate(createdIssue.getDueDate());
+            issueDTO.setPriority(createdIssue.getPriority());
+            issueDTO.setPriority(createdIssue.getPriority());
+            issueDTO.setProjectId(createdIssue.getProjectId());
+            issueDTO.setStatus(createdIssue.getStatus());
+            issueDTO.setTags(createdIssue.getTags());
+
+            return ResponseEntity.ok(issueDTO);
+    }
+
+    @DeleteMapping("/{issueId}")
+    public ResponseEntity<MessageResponse> deleteIssueById(@PathVariable Long issueId,
+                                                        @RequestHeader("Authorization") String token)
+                throws Exception{
+
+        User user = userService.findUserProfileByJwt(token);
+        issueService.deleteIssue(issueId, user.getId());
+        MessageResponse resp = new MessageResponse();
+        resp.setMessage("Issue deleted");
+
+        return ResponseEntity.ok(resp);
+    }
+
+    @PutMapping("/{issueId}/assignee/{userId}")
+    public ResponseEntity<Issue> addUserToIssue(@PathVariable Long issueId,
+                                                @PathVariable Long userId)
+        throws Exception{
+
+        Issue issue = issueService.addUserToIssue(issueId, userId);
+        return ResponseEntity.ok(issue);
+    }
+
+    @PutMapping("/{issueId}/status/{status}")
+    public ResponseEntity<Issue> updateIssueStatus(@PathVariable Long issueId,
+                                                   @PathVariable String status)
+        throws Exception{
+        Issue issue = issueService.updateStatus(issueId, status);
+        return ResponseEntity.ok(issue);
     }
 
 
---
+++ projectManagementSystem/src/main/java/com/maverick/projectManagementSystem/model/dto/IssueDTO.java
@@ -0,0 +1,28 @@
+package com.maverick.projectManagementSystem.model.dto;
+
+import com.maverick.projectManagementSystem.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IssueDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private Long projectId;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
+
+    //Exclude assignee during serialization
+    private User assignee;
+}
